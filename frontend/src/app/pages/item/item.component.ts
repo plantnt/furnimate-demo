1f18d599011a146c1698532f7f7f6897dccc096c
DESKTOP-EDFP8D1\Janus <godzillagmk2204@gmail.com>
2023-09-18 19:53:09 -0500

    modified:   frontend/src/app/app.component.css 	modified:   frontend/src/app/app.module.ts 	new file:   frontend/src/app/item.service.spec.ts 	new file:   frontend/src/app/item.service.ts 	modified:   frontend/src/app/pages/catalog/catalog.component.css 	modified:   frontend/src/app/pages/catalog/catalog.component.html 	modified:   frontend/src/app/pages/item/item.component.html 	modified:   frontend/src/app/pages/item/item.component.ts 	renamed:    frontend/src/assets/img/large_91gc6ymf_ZLL_AC_SL_1500_1b51d82110.jpg -> frontend/src/assets/img/product_b.jpg 	renamed:    frontend/src/assets/img/armario_azul_restaurado_pintura_chalk_pain_1550493038_9b67fd3c61.jpg -> frontend/src/assets/img/product_c.jpg

--- frontend/src/app/pages/item/item.component.ts
+++ frontend/src/app/pages/item/item.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService, Item } from 'src/app/item.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit{
+  items: Item[] = []
 
+  constructor(private itemService: ItemService){
+
+  }
+
+  ngOnInit(): void {
+    this.itemService.getItems().subscribe((response) => {
+      console.log('API data:', response)
+      if (Array.isArray(response)) {
+        this.items = response;
+      } else {
+        this.items = [response];
+      }
+    })
+  }
 }
+
