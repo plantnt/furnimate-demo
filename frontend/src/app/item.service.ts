297418cd039b2c2f9e7e2f5985a3887a88b96aa5
plantnt <godzillagmk2204@gmail.com>
2023-09-19 07:10:50 -0500

    new file:   frontend/src/app/item.service.spec.ts 	new file:   frontend/src/app/item.service.ts 	modified:   frontend/src/app/pages/catalog/catalog.component.css 	modified:   frontend/src/app/pages/catalog/catalog.component.html 	modified:   frontend/src/app/pages/item/item.component.css 	modified:   frontend/src/app/pages/item/item.component.html 	modified:   frontend/src/app/pages/item/item.component.ts 	renamed:    frontend/src/assets/img/armario_azul_restaurado_pintura_chalk_pain_1550493038_9b67fd3c61.jpg -> frontend/src/assets/img/product_b.jpg 	renamed:    frontend/src/assets/img/large_91gc6ymf_ZLL_AC_SL_1500_1b51d82110.jpg -> frontend/src/assets/img/product_c.jpg

---
+++ frontend/src/app/item.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs"
+
+export interface Item{
+  title: string
+  description: string
+  price: number
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  // private apiUrl = 'http://localhost:1337'
+  // constructor(private http: HttpClient) { }
+
+  // getItems(): Observable<Item[]>{
+  //   return this.http.get<Item[]>(`${this.apiUrl}/api/items`)
+  // }
+}
--- frontend/src/app/pages/item/item.component.ts
+++ frontend/src/app/pages/item/item.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { ItemService, Item } from 'src/app/item.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit, AfterViewInit{
+  items: Item[] = []
+  @ViewChild('itemImage')
+  itemImg!: ElementRef
 
+  constructor(private itemService: ItemService){
+
+  }
+
+  ngAfterViewInit(): void {
+    const itemOptions = ["a.jpg", "b.jpg", "c.jpg"]
+    const randomOption = itemOptions[Math.floor(Math.random()*itemOptions.length)]
+
+    if (this.itemImg && this.itemImg.nativeElement) {
+      this.itemImg.nativeElement.src = `assets/img/product_${randomOption}`;
+    }
+  }
+
+  ngOnInit(): void {
+    // this.itemService.getItems().subscribe((response) => {
+    //   console.log('API data:', response)
+    //   if (Array.isArray(response)) {
+    //     this.items = response;
+    //   } else {
+    //     this.items = [response];
+    //   }
+    // })
+  }
 }
