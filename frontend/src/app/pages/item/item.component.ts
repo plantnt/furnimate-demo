f6818d8d3ca7741230a20ae71f140fdcb1f43872
DESKTOP-EDFP8D1\Janus <godzillagmk2204@gmail.com>
2023-09-20 16:13:55 -0500

    modified:   frontend/src/app/pages/item/item.component.css 	modified:   frontend/src/app/pages/item/item.component.html 	modified:   frontend/src/app/pages/item/item.component.ts 	new file:   frontend/src/assets/img/product_d.jpg 	new file:   frontend/src/assets/img/product_e.jpg 	new file:   frontend/src/assets/img/product_f.jpg 	new file:   frontend/src/assets/img/product_g.jpg

--- frontend/src/app/pages/item/item.component.ts
+++ frontend/src/app/pages/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { ItemService, Item } from 'src/app/item.service';
 
 @Component({
@@ -6,22 +6,37 @@ import { ItemService, Item } from 'src/app/item.service';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit{
-  items: Item[] = []
+
+
+export class ItemComponent implements OnInit, AfterViewInit{
+  // items: Item[] = []
+  @ViewChild('itemImg')
+  itemImg!: ElementRef;
+
 
   constructor(private itemService: ItemService){
-
+    
   }
 
+
   ngOnInit(): void {
-    this.itemService.getItems().subscribe((response) => {
-      console.log('API data:', response)
-      if (Array.isArray(response)) {
-        this.items = response;
-      } else {
-        this.items = [response];
-      }
-    })
+    // this.itemService.getItems().subscribe((response) => {
+    //   console.log('API data:', response)
+    //   if (Array.isArray(response)) {
+    //     this.items = response;
+    //   } else {
+    //     this.items = [response];
+    //   }
+    // })
+
+    
+  }
+  ngAfterViewInit(): void {
+    const imageOptions = ["a.jpg","b.jpg","c.jpg","d.jpg","e.jpg","f.jpg","g.jpg"]
+    const randomImg = imageOptions[Math.floor(Math.random()*imageOptions.length)]
+    if (this.itemImg && this.itemImg.nativeElement) {
+      this.itemImg.nativeElement.src = `assets/img/product_${randomImg}`;
+    }
   }
 }
 
