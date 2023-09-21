1e69affac35bbe121075e90f956723d43b48b666
mixsw <143354988+mixswayk@users.noreply.github.com>
2023-09-21 15:52:38 -0500

    Reparación completa (supongo)
    Colocación de contenido temporal en <catalog>, <cart-pages> y <profile>.
    
    Para ser partidario del código limpio, los links largos se van al app.component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './pages/cart-pages/cart/cart.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { ProductComponent } from './pages/product/product.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
-{ path: '', component: CatalogComponent, title: 'Catalogo'},
+{ path: '', component: CatalogComponent, title: 'Cátalogo'},
 { path: 'producto', component: ProductComponent, title: 'Producto' },
 { path: 'carrito', component: CartComponent, title: 'Carrito'},
+{ path: 'perfil', component: ProfileComponent, title: 'Perfil de usuario'}
 ];
 
 @NgModule({
