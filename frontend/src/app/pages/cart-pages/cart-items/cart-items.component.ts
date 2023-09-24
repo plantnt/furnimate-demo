7162cb6165255c5034e57c63a61b4ddee960daf9
tgodzila22 <tgodzila22@gmail.com>
2023-09-24 16:38:53 -0500

    avanzando en el carrito nesecito ayuda con los items

--- frontend/src/app/pages/cart-pages/cart-items/cart-items.component.ts
+++ frontend/src/app/pages/cart-pages/cart-items/cart-items.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CartItemsComponent {
   cartimg="https://th.bing.com/th/id/OIP.jX55BYOHPRrWCd7zzBsSSgHaGm?w=191&h=180&c=7&r=0&o=5&pid=1.7";
+  
+
 }
