2e2be588e4ce850c44688f476b214db23f4a88fc
plantnt <godzillagmk2204@gmail.com>
2023-09-26 08:12:33 -0500

    modified:   frontend/src/app/pages/product/product.component.html 	modified:   frontend/src/app/pages/product/product.component.ts

--- frontend/src/app/pages/product/product.component.ts
+++ frontend/src/app/pages/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild  } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -6,15 +6,5 @@ import { Component, OnInit, AfterViewInit, ElementRef, ViewChild  } from '@angul
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  @ViewChild('productImage')
-  productImg!: ElementRef
 
-  ngAfterViewInit(): void {
-    const itemOptions = ["a", "b","c","d","e","f","g","h","i","j","k","l"]
-    const randomOption = itemOptions[Math.floor(Math.random()*itemOptions.length)]
-
-    if (this.productImg && this.productImg.nativeElement) {
-      this.productImg.nativeElement.src = `assets/img/products/product_${randomOption}.jpg`;
-    }
-  }
 }
