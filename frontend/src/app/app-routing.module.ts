c9724309f7376113d025cab9047ea3ee7b0efdac
mixsw <143354988+mixswayk@users.noreply.github.com>
2023-09-26 17:34:54 -0500

    LOGIN + REGISTER
    Mayoría de botones incluídos, ya hay un botón provisional en el header para entrar a todo esto, y herrera a arreglar el carrito ome

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { CartComponent } from './pages/cart-pages/cart/cart.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
 { path: '', component: CatalogComponent, title: 'Cátalogo'},
 { path: 'producto', component: ProductComponent, title: 'Producto' },
 { path: 'carrito', component: CartComponent, title: 'Carrito'},
-{ path: 'perfil', component: ProfileComponent, title: 'Perfil de usuario'}
+{ path: 'perfil', component: ProfileComponent, title: 'Perfil de usuario'},
+{ path: 'login', component: LoginComponent, title: 'Inicio de sesión'},
+{ path: 'register', component: RegisterComponent, title: 'Registro de cuenta'},
 ];
 
 @NgModule({
